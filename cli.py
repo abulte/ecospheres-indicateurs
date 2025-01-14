2ea1c9f6ecb51dab4b2f6e2650a2baa9e0d10b3c
Alexandre Bulté <alexandre@bulte.net>
2025-01-14 08:27:02 +0100

    add slugify-filters

--- cli.py
+++ cli.py
@@ -1,10 +1,13 @@
 import os
 
+from pathlib import Path
+
 import commentjson
 import requests
 import yaml
 
 from minicli import cli, run
+from slugify import slugify
 
 
 @cli
@@ -56,6 +59,24 @@ def fixtures(env: str = "demo", org_slug: str = "ecospheres-indicateurs-test"):
             )
             r.raise_for_status()
             print(f"Updated existing dataset: {dataset['id']}")
+
+
+@cli
+def slugify_filters(
+    input_file: Path = Path('filters/filters.yml'),
+    output_file: Path = Path('filters/filters-slugified.yml'),
+):
+    with open(input_file, 'r', encoding='utf-8') as file:
+        data = yaml.safe_load(file)
+
+    for filter_item in data['indicators']['filters']:
+        if 'values' in filter_item:
+            for value in filter_item['values']:
+                if 'id' in value:
+                    value['id'] = slugify(value['id'])
+
+    with open(output_file, 'w', encoding='utf-8') as file:
+        yaml.dump(data, file, allow_unicode=True, sort_keys=False)
 
 
 if __name__ == "__main__":
